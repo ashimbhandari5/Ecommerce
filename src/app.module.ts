aa48925f2fde44e947db8e29c44c7ca6edddd232
ashimbhandari5 <ashimbhandari700@gmail.com>
2025-04-07 21:59:35 +0545

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RolesModule } from './roles/roles.module';
 import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [RolesModule],
+  imports: [RolesModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
