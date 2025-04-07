24f59b85c3bd203e9486cf288dd377468a2600ec
ashimbhandari5 <ashimbhandari700@gmail.com>
2025-04-07 21:59:05 +0545

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RolesModule } from './roles/roles.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [RolesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
