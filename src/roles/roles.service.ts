14bbe2e23dfe169eb5edacb2625fcd5d926f7205
ashimbhandari5 <ashimbhandari700@gmail.com>
2025-04-09 21:28:40 +0545

    .

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,30 +1,103 @@
-import { Injectable } from '@nestjs/common';
+// import { Injectable } from '@nestjs/common';
+// import { CreateRoleDto } from './dto/create-role.dto';
+// import { UpdateRoleDto } from './dto/update-role.dto';
+// import { PrismaService } from 'src/prisma/prisma.service';
+
+// @Injectable()
+// export class RolesService {
+//   constructor(private readonly prismaService: PrismaService) {}
+//   async create(createRoleDto: CreateRoleDto) {
+//     return this.prismaService.role.create({
+//       data: createRoleDto,
+//     });
+//   }
+
+//   findAll() {
+//     return `This action returns all roles`;
+//   }
+
+//   findOne(id: number) {
+//     return `This action returns a #${id} role`;
+//   }
+
+//   update(id: number, updateRoleDto: UpdateRoleDto) {
+//     return `This action updates a #${id} role`;
+//   }
+
+//   remove(id: number) {
+//     return `This action removes a #${id} role`;
+//   }
+// }
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
-import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class RolesService {
+export class RoleService {
   constructor(private readonly prismaService: PrismaService) {}
+
   async create(createRoleDto: CreateRoleDto) {
     return this.prismaService.role.create({
       data: createRoleDto,
     });
   }
 
-  findAll() {
-    return `This action returns all roles`;
+  async findAll() {
+    return this.prismaService.role.findMany({
+      include: {
+        users: true,
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} role`;
+  async findOne(id: number) {
+    const role = await this.prismaService.role.findUnique({
+      where: { id },
+      include: {
+        users: true,
+      },
+    });
+    if (!role) {
+      throw new NotFoundException(`Role with ID ${id} not found`);
+    }
+    return role;
   }
 
-  update(id: number, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
+  async update(id: number, updateRoleDto: UpdateRoleDto) {
+    await this.checkIfRoleExist(id);
+    return this.prismaService.role.update({
+      where: { id },
+      data: updateRoleDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} role`;
+  async remove(id: number) {
+    await this.checkIfRoleExist(id);
+    return this.prismaService.role.delete({
+      where: { id },
+    });
+  }
+
+  private async getRoleById(id: number) {
+    const role = await this.prismaService.role.findFirst({
+      where: { id },
+    });
+
+    if (!role) {
+      throw new NotFoundException(`Role with ID ${id} does not exist`);
+    }
+    return role;
+  }
+
+  private async checkIfRoleExist(id: number): Promise<boolean> {
+    const role = await this.prismaService.role.findUnique({
+      where: { id },
+    });
+
+    if (!role) {
+      throw new NotFoundException(`Role with ID ${id} does not exist`);
+    }
+    return true;
   }
 }
